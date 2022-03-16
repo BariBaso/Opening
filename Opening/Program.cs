b4d00d07b0e5b8ee70a008e42db9559c500d8f22
BariBaso <kissmarci41@gmail.com>
2022-03-16 12:50:57 +0100

    Add project files.

---
+++ Opening/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Opening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
